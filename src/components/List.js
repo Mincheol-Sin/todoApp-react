a31e915d5a016255f01c31d1c49f7b961fb67b5e
Mincheol Sin <106597531+Mincheol-Sin@users.noreply.github.com>
2022-11-10 19:53:49 +0900

    Feat: Drag and Drop 기능 추가
    - react-beautiful-dnd 라이브러리 활용

--- src/components/List.js
+++ src/components/List.js
@@ -1,23 +1,7 @@
 import React from "react";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 
 export default function List({ todoData, setTodoData }) {
-  const btnStyle = {
-    color: "#fff",
-    border: "none",
-    padding: "5px 9px",
-    borderRadius: "50%",
-    cursor: "pointer",
-    float: "right",
-  };
-
-  const getStyle = (completed) => {
-    return {
-      padding: "10px",
-      borderBottom: "1px #ccc dotted",
-      textDecoration: completed ? "line-through" : "none",
-    };
-  };
-
   const handleCompleChange = (id) => {
     let newTodoData = todoData.map((data) => {
       if (data.id === id) {
@@ -34,21 +18,67 @@ export default function List({ todoData, setTodoData }) {
     setTodoData(newTodoData);
   };
 
+  const handleEnd = (result) => {
+    if (!result.destination) return;
+    const newTodoData = todoData;
+
+    // 1. 변경시키는 아이템을 배열에서 지워줍니다.
+    const [reorderedItem] = newTodoData.splice(result.source.index, 1);
+    // 2. return 값으로 지워진 아이템을 잡아줍니다.
+    newTodoData.splice(result.destination.idnex, 0, reorderedItem);
+
+    // 원하는 자리에 reorderedItem을 삽입합니다.
+    setTodoData(newTodoData);
+  };
+
   return (
     <div>
-      {todoData.map((data) => (
-        <div style={getStyle(data.completed)} key={data.id}>
-          <input
-            type="checkbox"
-            onChange={() => handleCompleChange(data.id)}
-            defaultChecked={false}
-          />
-          {data.title}
-          <button style={btnStyle} onClick={() => handleClick(data.id)}>
-            x
-          </button>
-        </div>
-      ))}
+      <DragDropContext onDragEnd={handleEnd}>
+        <Droppable droppableId="todo">
+          {(provided) => (
+            <div {...provided.droppableProps} ref={provided.innerRef}>
+              {todoData.map((data, index) => (
+                <Draggable
+                  key={data.id}
+                  draggableId={data.id.toString()}
+                  index={index}
+                >
+                  {(provided, snapshot) => (
+                    <div
+                      key={data.id}
+                      {...provided.draggableProps}
+                      ref={provided.innerRef}
+                      {...provided.dragHandleProps}
+                      className={`${
+                        snapshot.isDragging ? "bg-gray-400" : "bg-gray-100"
+                      } flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 border rounded`}
+                    >
+                      <div className="items-center">
+                        <input
+                          type="checkbox"
+                          onChange={() => handleCompleChange(data.id)}
+                          defaultChecked={false}
+                        />{" "}
+                        <span
+                          className={
+                            data.completed ? "line-through" : undefined
+                          }
+                        >
+                          {data.title}
+                        </span>
+                      </div>
+                      <div className="items-center">
+                        <button onClick={() => handleClick(data.id)}>x</button>
+                      </div>
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
     </div>
   );
 }
